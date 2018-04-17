429861679e549693a853c6d64ae132481e48da78
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-04-17 10:39:04 -0300

    gerando arquivo ppt 2

--- first.php
+++ first.php
@@ -1,50 +1,91 @@
 <?php
 
-require_once 'vendor/autoload.php'; //autoload do composer
+// Autoload do composer.
+require_once 'vendor/autoload.php';
 
-use PhpOffice\PhpPresentation\PhpPresentation; //classe do PhpPresentation
-use PhpOffice\PhpPresentation\IOFactory; //classe para manipular os arquivos
-use PhpOffice\PhpPresentation\Style\Color; //classe de estilo de cores
-use PhpOffice\PhpPresentation\Style\Alignment; //classe de estilo de alinhamentos
+// Classe do PhpPresentation.
+use PhpOffice\PhpPresentation\IOFactory;
+// Classe para manipular os arquivos.
+use PhpOffice\PhpPresentation\PhpPresentation;
+// Classe de estilo de cores.
+use PhpOffice\PhpPresentation\Style\Alignment;
+// Classe de estilo de alinhamentos.
+use PhpOffice\PhpPresentation\Style\Color;
 
-$objPHPPowerPoint = new PhpPresentation(); //instanciando uma nova apresentação
+// Instanciando uma nova apresentação.
+$objPHPPowerPoint = new PhpPresentation();
 
-$currentSlide = $objPHPPowerPoint->getActiveSlide(); //retornando o slide ativo
+// Retornando o slide ativo.
+$currentSlide = $objPHPPowerPoint->getActiveSlide();
 
-$shape = $currentSlide->createDrawingShape(); //criando uma forma de desenho (imagem)
-$shape->setName('PokePHP Logo') //definindo o nome da forma
-      ->setDescription('PokePHP Logo') //definindo a descrição da forma
-      ->setPath('./images/pokephp_logo.png') //definindo o  logo no topo do slide
-      ->setHeight(36) //definindo a altura da forma
-      ->setOffsetX(10) //definindo as coordenadas do eixo X referente a posição da forma
-      ->setOffsetY(10); //definindo as coordenadas do eixo Y referente a posição da forma
-$shape->getShadow()->setVisible(true) //definindo uma sombra na imagem
-                   ->setDirection(45) //definindo a direção da sombra
-                   ->setDistance(10); //definindo a distancia da sombra
+// Criando uma forma de desenho (imagem).
+$shape = $currentSlide->createDrawingShape();
+// Definindo o nome da forma.
+$shape->setName('PokePHP Logo')
+// Definindo a descrição da forma.
+    ->setDescription('PokePHP Logo')
+// Definindo o  logo no topo do slide.
+    ->setPath('./images/pokephp_logo.png')
+// Definindo a altura da forma.
+    ->setHeight(36)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(10)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(10);
+// Definindo uma sombra na imagem.
+$shape->getShadow()->setVisible(true)
+// Definindo a direção da sombra.
+    ->setDirection(45)
+// Definindo a distancia da sombra.
+    ->setDistance(10);
 
-$shape = $currentSlide->createRichTextShape() //criando uma forma (texto)
-      ->setHeight(300) //definindo a altura da forma
-      ->setWidth(600) //definindo a largura da forma
-      ->setOffsetX(170) //definindo as coordenadas do eixo X referente a posição da forma
-      ->setOffsetY(200); //definindo as coordenadas do eixo Y referente a posição da forma
-$shape->getActiveParagraph()->getAlignment()->setHorizontal( Alignment::HORIZONTAL_CENTER ); //definindo o alinhamento do paragrafo
-$textRun = $shape->createTextRun('Stand-Up do Pokemãobr!'); //definindo o texto a ser escrito
-$textRun->getFont()->setBold(true) //definindo a fonte como negrito
-                   ->setSize(60) //definindo o tamanho da fonte
-                   ->setColor( new Color( 'FFE06B20' ) ); //definindo a cor da fonte
+// Criando uma forma (texto).
+$shape = $currentSlide->createRichTextShape()
+// Definindo a altura da forma.
+    ->setHeight(300)
+// Definindo a largura da forma.
+    ->setWidth(600)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(170)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(200);
+// Definindo o alinhamento do paragrafo.
+$shape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+// Definindo o texto a ser escrito.
+$textRun = $shape->createTextRun('Stand-Up do Pokemãobr!');
+// Definindo a fonte como negrito.
+$textRun->getFont()->setBold(true)
+// Definindo o tamanho da fonte.
+    ->setSize(60)
+// Definindo a cor da fonte.
+    ->setColor(new Color('FFE06B20'));
 
-$shape = $currentSlide->createRichTextShape() //criando uma forma (texto)
-      ->setHeight(100) //definindo a altura da forma
-      ->setWidth(600) //definindo a largura da forma
-      ->setOffsetX(10)//definindo as coordenadas do eixo X referente a posição da forma
-      ->setOffsetY(640); //definindo as coordenadas do eixo Y referente a posição da forma
-$shape->getActiveParagraph()->getAlignment()->setHorizontal( Alignment::HORIZONTAL_LEFT ); //definindo o alinhamento do paragrafo
-$textRun = $shape->createTextRun('@pokemaobr'); //definindo o texto a ser escrito
-$textRun->getFont()->setBold(true) //definindo a fonte como negrito
-                   ->setSize(20) //definindo o tamanho da fonte
-                   ->setColor( new Color( '555555' ) ); //definindo a cor da fonte
+// Criando uma forma (texto).
+$shape = $currentSlide->createRichTextShape()
+// Definindo a altura da forma.
+    ->setHeight(100)
+// Definindo a largura da forma.
+    ->setWidth(600)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(10)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(640);
+// Definindo o alinhamento do paragrafo.
+$shape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+// Definindo o texto a ser escrito.
+$textRun = $shape->createTextRun('@pokemaobr');
+// Definindo a fonte como negrito.
+$textRun->getFont()->setBold(true)
+// Definindo o tamanho da fonte.
+    ->setSize(20)
+// Definindo a cor da fonte.
+    ->setColor(new Color('555555'));
 
-$oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007'); //definindo o tipo de arquivo como PowerPoint2007
-$oWriterPPTX->save(__DIR__ . "/exemplo1.pptx"); //definindo o nome de arquivo de saída
-$oWriterODP = IOFactory::createWriter($objPHPPowerPoint, 'ODPresentation'); //definindo o tipo de arquivo como Open Document
-$oWriterODP->save(__DIR__ . "/exemplo1.odp"); //definindo o nome de arquivo de saída
+// Definindo o tipo de arquivo como PowerPoint2007.
+$oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
+// Definindo o nome de arquivo de saída.
+$oWriterPPTX->save(__DIR__ . "/exemplo1.pptx");
+// Definindo o tipo de arquivo como Open Document.
+$oWriterODP = IOFactory::createWriter($objPHPPowerPoint, 'ODPresentation');
+// Definindo o nome de arquivo de saída.
+$oWriterODP->save(__DIR__ . "/exemplo1.odp");
---
+++ second.php
@@ -0,0 +1,229 @@
+<?php
+
+// Autoload do composer.
+require_once 'vendor/autoload.php';
+
+// Classe do PhpPresentation.
+use PhpOffice\PhpPresentation\IOFactory;
+// Classe para manipular os arquivos.
+use PhpOffice\PhpPresentation\PhpPresentation;
+// Classe de estilo de cores.
+use PhpOffice\PhpPresentation\Style\Alignment;
+// Classe de estilo de alinhamentos.
+use PhpOffice\PhpPresentation\Style\Color;
+
+// Instanciando uma nova apresentação.
+$objPHPPowerPoint = new PhpPresentation();
+// Retornando o slide ativo.
+$currentSlide = $objPHPPowerPoint->getActiveSlide();
+
+// Criando uma forma de desenho (imagem).
+$shape = $currentSlide->createDrawingShape();
+// Definindo o nome da forma.
+$shape->setName('PokePHP Logo')
+
+// Definindo a descrição da forma.
+    ->setDescription('PokePHP Logo')
+// Definindo o  logo no topo do slide.
+    ->setPath('./images/pokephp_logo.png')
+// Definindo a altura da forma.
+    ->setHeight(36)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(10)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(10);
+
+// Definindo uma sombra na imagem.
+$shape->getShadow()->setVisible(true)
+// Definindo a direção da sombra.
+    ->setDirection(45)
+// Definindo a distancia da sombra.
+    ->setDistance(10);
+
+// Criando uma forma (texto).
+$shape = $currentSlide->createRichTextShape()
+// Definindo a altura da forma.
+    ->setHeight(300)
+// Definindo a largura da forma.
+    ->setWidth(600)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(170)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(200);
+// Definindo o alinhamento do paragrafo.
+$shape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+// Definindo o texto a ser escrito.
+$textRun = $shape->createTextRun('Stand-Up do Pokemãobr!');
+// Definindo a fonte como negrito.
+$textRun->getFont()->setBold(true)
+// Definindo o tamanho da fonte.
+    ->setSize(60)
+// Definindo a cor da fonte.
+    ->setColor(new Color('FFE06B20'));
+
+// Criando uma forma (texto).
+$shape = $currentSlide->createRichTextShape()
+// Definindo a altura da forma.
+    ->setHeight(100)
+// Definindo a largura da forma.
+    ->setWidth(600)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(10)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(640);
+
+// Definindo o alinhamento do paragrafo.
+$shape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+// Definindo o texto a ser escrito.
+$textRun = $shape->createTextRun('@pokemaobr');
+// Definindo a fonte como negrito.
+$textRun->getFont()->setBold(true)
+// Definindo o tamanho da fonte.
+    ->setSize(20)
+// Definindo a cor da fonte.
+    ->setColor(new Color('555555'));
+
+// Criando um novo slide.
+$currentSlide = $objPHPPowerPoint->createSlide();
+
+// Criando uma forma de desenho (imagem).
+$shape = $currentSlide->createDrawingShape();
+// Definindo o nome da forma.
+$shape->setName('PokePHP Logo')
+// Definindo a descrição da forma.
+    ->setDescription('PokePHP Logo')
+// Definindo o  logo no topo do slide.
+    ->setPath('./images/pokephp_logo.png')
+// Definindo a altura da forma.
+    ->setHeight(36)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(10)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(10);
+// Definindo uma sombra na imagem.
+$shape->getShadow()->setVisible(true)
+// Definindo a direção da sombra.
+    ->setDirection(45)
+// Definindo a distancia da sombra.
+    ->setDistance(10);
+
+//  Create a shape (drawing).
+
+// Criando uma forma de desenho (imagem).
+$shape = $currentSlide->createDrawingShape();
+// Definindo o nome da forma.
+$shape->setName('Por que PHP')
+
+// Definindo a descrição da forma.
+    ->setDescription('Por que PHP')
+// Definindo o  logo no topo do slide.
+    ->setPath('./images/porque_php.png')
+// Definindo a altura da forma.
+    ->setHeight(500)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(10)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(10);
+// Definindo uma sombra na imagem.
+$shape->getShadow()->setVisible(true)
+// Definindo a direção da sombra.
+    ->setDirection(45)
+// Definindo a distancia da sombra.
+    ->setDistance(10);
+
+// Criando uma forma (texto).
+$shape = $currentSlide->createRichTextShape()
+// Definindo a altura da forma.
+    ->setHeight(100)
+// Definindo a largura da forma.
+    ->setWidth(600)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(10)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(640);
+// Definindo o alinhamento do paragrafo.
+$shape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+// Definindo o texto a ser escrito.
+$textRun = $shape->createTextRun('@pokemaobr');
+// Definindo a fonte como negrito.
+$textRun->getFont()->setBold(true)
+// Definindo o tamanho da fonte.
+    ->setSize(20)
+// Definindo a cor da fonte.
+    ->setColor(new Color('555555'));
+
+// Criando um novo slide.
+$currentSlide = $objPHPPowerPoint->createSlide();
+
+// Criando uma forma de desenho (imagem).
+$shape = $currentSlide->createDrawingShape();
+// Definindo o nome da forma.
+$shape->setName('PokePHP Logo')
+// Definindo a descrição da forma.
+    ->setDescription('PokePHP Logo')
+// Definindo o  logo no topo do slide.
+    ->setPath('./images/pokephp_logo.png')
+// Definindo a altura da forma.
+    ->setHeight(36)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(10)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(10);
+
+// Definindo uma sombra na imagem.
+$shape->getShadow()->setVisible(true)
+// Definindo a direção da sombra.
+    ->setDirection(45)
+// Definindo a distancia da sombra.
+    ->setDistance(10);
+
+// Criando uma forma (texto).
+$shape = $currentSlide->createRichTextShape()
+// Definindo a altura da forma.
+    ->setHeight(300)
+// Definindo a largura da forma.
+    ->setWidth(600)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(170)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(200);
+// Definindo o alinhamento do paragrafo.
+$shape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+// Definindo o texto a ser escrito.
+$textRun = $shape->createTextRun('Obrigado!');
+// Definindo a fonte como negrito.
+$textRun->getFont()->setBold(true)
+// Definindo o tamanho da fonte.
+    ->setSize(60)
+// Definindo a cor da fonte.
+    ->setColor(new Color('FFE06B20'));
+
+// Criando uma forma (texto).
+$shape = $currentSlide->createRichTextShape()
+// Definindo a altura da forma.
+    ->setHeight(100)
+// Definindo a largura da forma.
+    ->setWidth(600)
+// Definindo as coordenadas do eixo X referente a posição da forma.
+    ->setOffsetX(10)
+// Definindo as coordenadas do eixo Y referente a posição da forma.
+    ->setOffsetY(640);
+// Definindo o alinhamento do paragrafo.
+$shape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+// Definindo o texto a ser escrito.
+$textRun = $shape->createTextRun('@pokemaobr');
+// Definindo a fonte como negrito.
+$textRun->getFont()->setBold(true)
+// Definindo o tamanho da fonte.
+    ->setSize(20)
+// Definindo a cor da fonte.
+    ->setColor(new Color('555555'));
+
+// Definindo o tipo de arquivo como PowerPoint2007.
+$oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
+// Definindo o nome de arquivo de saída.
+$oWriterPPTX->save(__DIR__ . "/example2.pptx");
+// Definindo o tipo de arquivo como Open Document.
+$oWriterODP = IOFactory::createWriter($objPHPPowerPoint, 'ODPresentation');
+// Definindo o nome de arquivo de saída.
+$oWriterODP->save(__DIR__ . "/example2.odp");
